ce12897e8e1cf946941ef43aebfac8ba67ddbaa4
ElianArism <elianarism@gmail.com>
2023-09-12 04:25:16 -0300

    new disto

--- dist/modules/auth/auth.controller.js
+++ dist/modules/auth/auth.controller.js
@@ -1,12 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.logout = exports.renewSession = exports.login = void 0;
+exports.logout = exports.profile = exports.renewSession = exports.login = void 0;
 const encription_utils_1 = require("../../utils/encription.utils");
 const get_user_repository_by_role_1 = require("../../utils/get-user-repository-by-role");
 const jwt_utils_1 = require("../../utils/jwt.utils");
 const login = async (req, res) => {
     const { id, password, role } = req.body;
-    console.log(req.body);
     try {
         const collection = (0, get_user_repository_by_role_1.getUserCollectionByRole)(role);
         if (!collection) {
@@ -54,6 +53,44 @@ const login = async (req, res) => {
 exports.login = login;
 const renewSession = async (req, res) => { };
 exports.renewSession = renewSession;
+const profile = async (req, res) => {
+    const id = req.id;
+    const role = req.role;
+    try {
+        const collection = (0, get_user_repository_by_role_1.getUserCollectionByRole)(role);
+        if (!collection) {
+            return res.status(400).json({
+                ok: false,
+                error: {
+                    message: `Param :role does not exists or it is invalid`,
+                },
+            });
+        }
+        const user = await collection.getById(id);
+        if (!user) {
+            return res.status(404).json({
+                ok: false,
+                error: {
+                    message: "User not found",
+                },
+            });
+        }
+        return res.json({
+            ok: true,
+            data: user,
+        });
+    }
+    catch (error) {
+        return res.status(error?.status ?? 500).json({
+            ok: false,
+            error: {
+                message: error?.message,
+                logs: error,
+            },
+        });
+    }
+};
+exports.profile = profile;
 const logout = async (req, res) => { };
 exports.logout = logout;
 //# sourceMappingURL=auth.controller.js.map
--- dist/modules/auth/auth.routes.js
+++ dist/modules/auth/auth.routes.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const validate_jwt_middleware_1 = require("../../middlewares/validate-jwt.middleware");
 const auth_controller_1 = require("./auth.controller");
 const authRoutes = (0, express_1.Router)();
 authRoutes.post("/login", [], auth_controller_1.login);
 authRoutes.get("/renew-session", []);
+authRoutes.get("/profile", [validate_jwt_middleware_1.validateJWT], auth_controller_1.profile);
 authRoutes.post("/logout", []);
 exports.default = authRoutes;
 //# sourceMappingURL=auth.routes.js.map
