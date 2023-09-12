c9d0afa0f998c29b0b381aefea94645751ad07fa
ElianArism <elianarism@gmail.com>
2023-09-12 04:01:49 -0300

    add profile endpoint

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,7 +7,7 @@ import { signJWT } from "../../utils/jwt.utils"
 
 export const login = async (req: Request, res: Response) => {
   const { id, password, role } = req.body as LoginUserDTO
-  console.log(req.body)
+
   try {
     const collection = getUserCollectionByRole(role as AvailableRoles)
 
@@ -56,5 +56,47 @@ export const login = async (req: Request, res: Response) => {
 }
 
 export const renewSession = async (req: Request, res: Response) => {}
+
+export const profile = async (req: Request, res: Response) => {
+  const id = req.id
+  const role = req.role
+
+  try {
+    const collection = getUserCollectionByRole(role as AvailableRoles)
+
+    if (!collection) {
+      return res.status(400).json({
+        ok: false,
+        error: {
+          message: `Param :role does not exists or it is invalid`,
+        },
+      })
+    }
+
+    const user = await collection.getById(id)
+
+    if (!user) {
+      return res.status(404).json({
+        ok: false,
+        error: {
+          message: "User not found",
+        },
+      })
+    }
+
+    return res.json({
+      ok: true,
+      data: user,
+    })
+  } catch (error: any) {
+    return res.status(error?.status ?? 500).json({
+      ok: false,
+      error: {
+        message: error?.message,
+        logs: error,
+      },
+    })
+  }
+}
 
 export const logout = async (req: Request, res: Response) => {}
--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
-import { login } from "./auth.controller"
+import { validateJWT } from "../../middlewares/validate-jwt.middleware"
+import { login, profile } from "./auth.controller"
 
 const authRoutes = Router()
 
 authRoutes.post("/login", [], login)
 authRoutes.get("/renew-session", [])
+authRoutes.get("/profile", [validateJWT], profile)
 authRoutes.post("/logout", [])
 
 export default authRoutes
