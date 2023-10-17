320ff77765fc3fb72c3055431dab23630c82a69c
ElianArism <elianarism@gmail.com>
2023-10-17 04:17:58 -0300

    add things

--- dist/utils/get-user-repository-by-role.js
+++ dist/utils/get-user-repository-by-role.js
@@ -4,7 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getUserCollectionByRole = void 0;
+const authority_model_1 = __importDefault(require("../db/models/authority.model"));
 const parent_model_1 = __importDefault(require("../db/models/parent.model"));
+const personal_model_1 = __importDefault(require("../db/models/personal.model"));
 const professor_model_1 = __importDefault(require("../db/models/professor.model"));
 const student_model_1 = __importDefault(require("../db/models/student.model"));
 const user_repository_1 = require("../db/repository/user.repository");
@@ -17,6 +19,12 @@ const getUserCollectionByRole = (role) => {
     }
     else if ("student" /* AvailableRoles.student */ === role) {
         return new user_repository_1.UserRepository(student_model_1.default);
+    }
+    else if ("authority" /* AvailableRoles.authority */ === role) {
+        return new user_repository_1.UserRepository(authority_model_1.default);
+    }
+    else if ("personal" /* AvailableRoles.personal */ === role) {
+        return new user_repository_1.UserRepository(personal_model_1.default);
     }
     return null;
 };
--- src/utils/get-user-repository-by-role.ts
+++ src/utils/get-user-repository-by-role.ts
@@ -1,9 +1,16 @@
+import Authority from "../db/models/authority.model"
 import Parent from "../db/models/parent.model"
+import Personal from "../db/models/personal.model"
 import Professor from "../db/models/professor.model"
 import Student from "../db/models/student.model"
 import { UserRepository } from "../db/repository/user.repository"
 import { AvailableRoles } from "../enums/roles.enum"
-import { IParent, IProfessor, IStudent } from "../interfaces/user.interface"
+import {
+  IParent,
+  IProfessor,
+  IStudent,
+  IUser,
+} from "../interfaces/user.interface"
 
 export const getUserCollectionByRole = (role: AvailableRoles) => {
   if (AvailableRoles.parent === role) {
@@ -12,6 +19,10 @@ export const getUserCollectionByRole = (role: AvailableRoles) => {
     return new UserRepository<IProfessor>(Professor)
   } else if (AvailableRoles.student === role) {
     return new UserRepository<IStudent>(Student)
+  } else if (AvailableRoles.authority === role) {
+    return new UserRepository<IUser>(Authority)
+  } else if (AvailableRoles.personal === role) {
+    return new UserRepository<IUser>(Personal)
   }
   return null
 }
